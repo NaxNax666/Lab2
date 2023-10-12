e731378d39970f761947473b6b272baa8646afd1
A.Galushkin <A.Galushkin@avilex.ru>
2023-10-13 01:44:13 +0300

    add logic

--- app/src/main/java/com/example/lab2/MainActivity.kt
+++ app/src/main/java/com/example/lab2/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.lab2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,21 +15,58 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lab2.ui.theme.Lab2Theme
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+fun make_solution(a:Int, b:Int, c:Int):String {
+    val root1: Double
+    val root2: Double
+    val output: String
+
+    val determinant = b * b - 4.0 * a * c
+
+    // condition for real and different roots
+    if (determinant > 0.0) {
+        root1 = (-b + Math.sqrt(determinant)) / (2 * a)
+        root2 = (-b - Math.sqrt(determinant)) / (2 * a)
+
+        output = "root1 = %.2f and root2 = %.2f".format(root1, root2)
+    }
+    else if (determinant == 0.0) {
+        root2 = -b / (2.0 * a)
+        root1 = root2
+
+        output = "root1 = root2 = %.2f;".format(root1)
+    }
+    // If roots are not real
+    else {
+        output = "Нет действительных корней"
+    }
+    return output
+
+
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Lab2Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        setContentView(R.layout.activity_main)
+        val aEditText = findViewById<EditText>(R.id.editTextA)
+        val bEditText = findViewById<EditText>(R.id.editTextB)
+        val cEditText = findViewById<EditText>(R.id.editTextC)
+        val textViewRes = findViewById<TextView>(R.id.textViewRes)
+
+
+        val nextButton = findViewById<Button>(R.id.buttonSolution)
+        nextButton.setOnClickListener {
+            textViewRes.setText("")
+            val a = aEditText.text.toString().toInt()
+            val b = bEditText.text.toString().toInt()
+            val c = cEditText.text.toString().toInt()
+            val res = make_solution(a, b, c)
+            textViewRes.append(res)
         }
+
     }
 }
 
